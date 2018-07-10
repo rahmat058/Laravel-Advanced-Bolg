419573e6e605e77168730e27fe17b74371c90459
Kazi Rahamatullah <kazirahmat58@gmail.com>
2018-07-11 01:50:48 +0600

    Multi Auth users and roles relation is working

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Default String length set to avoid the error of table||migrate insert in database 
+        Schema::defaultStringLength(191);
     }
 
     /**
